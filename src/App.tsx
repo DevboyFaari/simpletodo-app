c7294c685c2e7131f74d9d9a04b7d326c5629d2e
unknown <faarijoye@gmail.com>
2022-08-26 10:26:41 +0100

    removed a few lines

--- src/App.tsx
+++ src/App.tsx
@@ -59,44 +59,6 @@ const App: React.FC = () => {
     setTodos(active);
   };
 
-  // const handleAdd = (e: React.FormEvent) => {
-  //   e.preventDefault();
-
-  //   if (todo) {
-  //     setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-  //     setTodo("");
-  //   }
-  // };
-  // const onDragEnd = (result: DropResult) => {
-  //   const { source, destination } = result;
-
-  //   if (!destination) return;
-  //   if (
-  //     destination.droppableId === source.droppableId &&
-  //     destination.index === source.index
-  //   )
-  //     return;
-  //   let add,
-  //     active = todos,
-  //     complete = completedTodos;
-
-  //   if (source.droppableId === "TodosList") {
-  //     add = active[source.index];
-  //     active.splice(source.index, 1);
-  //   } else {
-  //     add = complete[source.index];
-  //     complete.splice(source.index, 1);
-  //   }
-
-  //   if (destination.droppableId === "TodosList") {
-  //     active.splice(destination.index, 0, add);
-  //   } else {
-  //     complete.splice(destination.index, 0, add);
-  //   }
-
-  //   setCompletedTodos(complete);
-  //   setTodos(active);
-  // };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement
-// );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 ReactDOM.render(
   <React.StrictMode>
     <App />
